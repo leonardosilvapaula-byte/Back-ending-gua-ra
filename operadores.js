3a56fee8219927407a3b5ed4a809e665d51d92e3
leonardosilvapaula-byte <leonardo.silva.paula@escola.pr.gov.br>
2026-03-31 17:41:36 +0000

    versao1

---
+++ operadores.js
@@ -0,0 +1,33 @@
+//OPRADOR E &&
+let maiorIdade = true;
+let possuiDocumento = true;
+
+if (maiorIdade && possuiDocumento) {
+    console.log('pode entrar na festa!')
+} else {
+    console.log('não pode entrar na festa.')
+};
+
+//OPERADOR OU ||
+let cartaoCredito = true; 
+let dinheiro = false; 
+
+if (cartaoCredito || dinheiro){
+    console.log('Pode fazer a compra.')
+} else {
+    console.log('Não pode fazer a compra.')
+};
+
+//OPRERADOR NOT !
+let estaChovendo = true;
+
+if (!estaChovendo){
+    console.log('pode sair sem guarda-chuva.')
+} else {
+    console.log('Leve o guarda-Chuva ')
+};
+
+//DENTRO DO TERMINAL NÃO COPIAR!!
+git add operadores 
+git commit -m "versao1"
+git push

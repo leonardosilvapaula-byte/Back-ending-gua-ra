8c7a6ff7431bd13d86421162e3fb73cd501d84d2
leonardosilvapaula-byte <leonardo.silva.paula@escola.pr.gov.br>
2026-04-14 17:34:41 +0000

    v1

---
+++ nivel.js
@@ -0,0 +1,8 @@
+function subirdenivel(nivelatual) {
+    return nivelatual + 1;
+}
+
+let meunivel = 10;
+let nivelatualizado = subirdenivel(meunivel); 
+
+console.log("seu nivel atual é: " + nivelatualizado);

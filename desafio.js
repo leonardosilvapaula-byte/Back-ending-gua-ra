52effc43bbc5a383ce3bfea00f8237765b573c91
leonardosilvapaula-byte <leonardo.silva.paula@escola.pr.gov.br>
2026-05-18 19:22:08 +0000

    versao2

---
+++ desafio.js
@@ -0,0 +1,13 @@
+const nome ="josesinho do peneu";
+let cargo = "Operador de caixa"
+let idade = 45;
+let salario = 1500.00;
+let empresa = "assai";
+
+console.log(`Qual seu nome? ${nome} Oque quer fazer na empresa? ${cargo} Qual sua idade? ${idade} Quanto quer receber? ${salario} Qual sua empresa? ${empresa}`);
+
+
+let bônus = 50.00;
+let soma = salario+bônus;
+
+console.log(`o salario total do meu funcionário mais o bônus é ${soma}`)
